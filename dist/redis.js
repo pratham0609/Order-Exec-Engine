527eac4fdca006c9b8f4b2e80931a516e8dbb827
pratham0609 <pndandale@hotmail.com>
2025-11-25 00:47:21 +0530

    deployment changes to support frontend

--- dist/redis.js
+++ dist/redis.js
@@ -7,6 +7,9 @@
 // });
 //////////////////////////////////////////////////////////////////
 import { Redis } from "ioredis";
+console.log("REDIS_URL from env:", process.env.REDIS_URL);
+console.log("REDIS_HOST:", process.env.REDIS_HOST);
+console.log("REDIS_PORT:", process.env.REDIS_PORT);
 const redisUrl = process.env.REDIS_URL;
 export const redis = redisUrl
     ? new Redis(redisUrl, {
